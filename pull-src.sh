cc1b98e59bf9e7157ede126db61c00b0f0064ac9
zombie <claney2005@protonmail.com>
2023-08-03 23:39:14 -0500

    Update pull-src.sh

--- pull-src.sh
+++ pull-src.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+mkdir watchdog/src
+mkdir admin/src
+mkdir discord/src
+mkdir nginx/src
 git clone https://github.com/space-wizards/SS14.Watchdog.git watchdog/src
 git clone --recurse-submodules https://github.com/space-wizards/SS14.Admin.git admin/src
-git clone https://gitlab.mythicaldimensions.me/mythical-dimensions/dss.git discord/src
+git clone https://github.com/mythical-dimensions/dss.git discord/src
+git clone https://github.com/mythical-dimensions/www.git nginx/src
