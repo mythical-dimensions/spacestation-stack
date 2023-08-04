2a49bb310d45d5d7333b0f6523774a1b4bd0b323
zombie <zombie@mythicaldimensions.me>
2023-08-03 23:13:01 -0500

    YEs

---
+++ pull-src.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone https://github.com/space-wizards/SS14.Watchdog.git watchdog/src
+git clone --recurse-submodules https://github.com/space-wizards/SS14.Admin.git admin/src
+git clone https://gitlab.mythicaldimensions.me/mythical-dimensions/dss.git discord/src
